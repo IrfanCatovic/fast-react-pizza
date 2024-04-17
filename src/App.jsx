fb592049a29b90cca5c22475026a1669dbcc51f4
IrfanCatovic <catovicc84@gmail.com>
2024-04-17 16:36:35 +0200

    loader added

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,31 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
 import Home from "./ui/Home";
-import Menu from "./features/menu/Menu";
+import Menu, { loader as menuLoader } from "./features/menu/Menu";
 import Cart from "./features/cart/Cart";
 import CreateOrder from "./features/order/CreateOrder";
 import Order from "./features/order/Order";
+import AppLayout from "./ui/AppLayout";
 
 const router = createBrowserRouter([
-  { path: "/", element: <Home /> },
-  { path: "/menu", element: <Menu /> },
-  { path: "/cart", element: <Cart /> },
-  { path: "/order/new", element: <CreateOrder /> },
-  { path: "/order/:orderId", element: <Order /> },
+  {
+    //AppLayout pravimo kao parent element otalim rutama jer zelimo da se na svakoj stranici prikazuje AppLayout
+    //To znaci da na svakoj ima header, Content i cartOverview, a izmedju njih ubacujemo sta zelimo da se vidi jos
+    element: <AppLayout />,
+    children: [
+      { path: "/", element: <Home /> },
+      { path: "/menu", element: <Menu />, loader: menuLoader },
+      { path: "/cart", element: <Cart /> },
+      { path: "/order/new", element: <CreateOrder /> },
+      { path: "/order/:orderId", element: <Order /> },
+    ],
+  },
+
+  // { path: "/", element: <Home /> },
+  // { path: "/menu", element: <Menu /> },
+  // { path: "/cart", element: <Cart /> },
+  // { path: "/order/new", element: <CreateOrder /> },
+  // { path: "/order/:orderId", element: <Order /> },
 ]);
 
 function App() {
--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,9 +1,18 @@
 import Header from "./Header";
+import CartOverview from "../features/cart/CartOverview";
+import { Outlet } from "react-router-dom";
 
 function AppLayout() {
   return (
     <div>
       <Header />
+
+      <main>
+        <h1>Content</h1>
+        <Outlet />
+      </main>
+
+      <CartOverview />
     </div>
   );
 }
