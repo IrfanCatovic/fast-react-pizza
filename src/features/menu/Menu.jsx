35688160fd54e618b3b829d6a2ef6b39933643c0
IrfanCatovic <catovicc84@gmail.com>
2024-04-17 17:22:14 +0200

    implement pizza card

--- src/features/menu/Menu.jsx
+++ src/features/menu/Menu.jsx
@@ -1,10 +1,17 @@
 import { useLoaderData } from "react-router-dom";
 import { getMenu } from "../../services/apiRestaurant";
+import MenuItem from "../menu/MenuItem";
 
 function Menu() {
   const menu = useLoaderData();
 
-  return <h1>Menu</h1>;
+  return (
+    <ul>
+      {menu.map((pizza) => (
+        <MenuItem pizza={pizza} key={pizza.id} />
+      ))}
+    </ul>
+  );
 }
 
 export async function loader() {
