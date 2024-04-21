e110b86537c5722834c2c81fe6c81f3be453a3f7
IrfanCatovic <catovicc84@gmail.com>
2024-04-21 16:54:02 +0200

    params planted

--- src/features/order/SearchOrder.jsx
+++ src/features/order/SearchOrder.jsx
@@ -1,16 +1,21 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 function SearchOrder() {
   const [query, setQuery] = useState("");
+  const navigate = useNavigate();
 
   function handleSubmit(e) {
     e.preventDefault();
+    if (!query) return;
+    navigate(`/order/${query}`);
+    setQuery("");
   }
 
   return (
     <form onSubmit={handleSubmit}>
       <input
-        placeholder="Search order#"
+        placeholder="Search order #"
         value={query}
         onChange={(e) => setQuery(e.target.value)}
       />
