94410ecd8e3c4187b30c2dc5f2c357019d4de33c
IrfanCatovic <catovicc84@gmail.com>
2024-05-01 20:49:34 +0200

    errors handle

--- src/features/order/UpdateOrder.jsx
+++ src/features/order/UpdateOrder.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable no-unused-vars */
 import { useFetcher } from 'react-router-dom';
 import Button from '../../ui/Button';
 import { updateOrder } from '../../services/apiRestaurant';
