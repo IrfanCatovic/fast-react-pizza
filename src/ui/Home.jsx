477a14fa5a2f4a892389cc01dbfea89058bab199
IrfanCatovic <catovicc84@gmail.com>
2024-05-05 17:08:28 +0200

    grid

--- src/ui/Home.jsx
+++ src/ui/Home.jsx
@@ -1,8 +1,8 @@
 import CreateUser from '../features/user/CreateUser'
 function Home() {
   return (
-    <div className=' sm:my-16 my-10 text-center'>
-      <h1 className="text-center text-xl font-semibold mb-8">
+    <div className='  my-10 px-4 text-center sm:my-16'>
+      <h1 className="text-center text-xl font-semibold mb-8 md:text-3xl">
         The best pizza.
         <br />
         <span className="text-yellow-500">
