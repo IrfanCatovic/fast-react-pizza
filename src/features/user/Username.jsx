834e60ee688547ec3d5aefaf0bba2b4c533be4df
IrfanCatovic <catovicc84@gmail.com>
2025-12-30 23:30:30 +0100

    kom

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,7 +1,8 @@
 function Username() {
     return (
-        <div className="text-sm font-semibold hidden md:block">
-            {/* Username goes here */}
+        <div className="text-sm font-semibold hidden md:block"> 
+            {/* Username goes here \
+            md: koristimo da za dizajn na telefon*/}
             Irfan
         </div>
     )
