2524ff26d65c293cf7fbc5a0dcad4d2394735d56
IrfanCatovic <catovicc84@gmail.com>
2026-01-06 21:39:59 +0100

    Improve link hover effect in Cart component and add console logs for form data in CreateOrder

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -114,6 +114,7 @@ export async function action({ request }) {
   const formData = await request.formData();
   const data = Object.fromEntries(formData);
 
+
   const order = {
     ...data,
     cart: JSON.parse(data.cart),
